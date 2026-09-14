48fe5be903913524bf7a0fee8be691cb09b70b42
fernando <fernanct21@gmail.com>
2026-09-14 00:57:51 -0500

    Mejora del container apps bug qa

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# 1. Referencias a la infraestructura existente (Laboratorio 1)
 data "azurerm_resource_group" "main" {
   name = "rg-cicd-terraform-app-${local.idapp}"
 }
@@ -7,21 +8,38 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
+# 2. Identidad Administrada independiente (Soluciona el error de lista vacía en principal_id)
+resource "azurerm_user_assigned_identity" "aca_identity" {
+  name                = "id-aca-${local.idapp}-${var.environment}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+# 3. Asignación del rol AcrPull sobre el ACR usando la identidad creada
+resource "azurerm_role_assignment" "aca_pull_default_acr" {
+  principal_id         = azurerm_user_assigned_identity.aca_identity.principal_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.acr.id
+}
+
+# 4. Entorno administrado de Container Apps
 resource "azurerm_container_app_environment" "aca_env" {
   name                = "aca-env-${local.idapp}-${var.environment}"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
-# resource "azurerm_resource_provider_registration" "app" {
-#   name = "Microsoft.App"
-# }
-
+# 5. Microservicio en Azure Container Apps
 resource "azurerm_container_app" "aca" {
   name                         = "aca-ms-${local.idapp}-${var.environment}"
   container_app_environment_id = azurerm_container_app_environment.aca_env.id
   resource_group_name          = data.azurerm_resource_group.main.name
   revision_mode                = "Single"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aca_identity.id]
+  }
 
   template {
     container {
@@ -30,10 +48,6 @@ resource "azurerm_container_app" "aca" {
       cpu    = 0.5
       memory = "1Gi"
     }
-  }
-
-  identity {
-    type = "SystemAssigned"
   }
 
   ingress {
@@ -45,7 +59,6 @@ resource "azurerm_container_app" "aca" {
       latest_revision = true
       percentage      = 100
     }
-
   }
 
   lifecycle {
@@ -54,15 +67,11 @@ resource "azurerm_container_app" "aca" {
       registry
     ]
   }
-  depends_on = [ azurerm_container_app_environment.aca_env ]
-}
 
-
-# Dar permisos al ACA para leer del ACR Global
-resource "azurerm_role_assignment" "aca_pull_default_acr" {
-  principal_id         = azurerm_container_app.aca.identity[0].principal_id
-  role_definition_name = "AcrPull"
-  scope                = data.azurerm_container_registry.acr.id
+  depends_on = [
+    azurerm_container_app_environment.aca_env,
+    azurerm_role_assignment.aca_pull_default_acr
+  ]
 }
 # resource "azurerm_container_registry" "acr" {
 #   name                = "acr${local.idapp}${var.environment}"
