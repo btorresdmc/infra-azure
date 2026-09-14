7af1ac93748ea3529eed9e2d76eaf5487cf56fb1
fernando <fernanct21@gmail.com>
2026-09-14 00:02:04 -0500

    fix: actualizar nombre

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-btorres" # Reemplazar por btorres
+    resource_group_name  = "rg-cicd-terraform-app-btorres" # Reemplazar por btorre
     storage_account_name = "tfstatebtorres"                # Reemplazar por btorres
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
